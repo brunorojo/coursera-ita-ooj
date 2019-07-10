dce69beebb3a557959c15c69a44e71cccda77775
brunorojo.WKS6100 <brunoasrojo@gmail.com>
2019-07-10 18:57:49 -0300

    include testcodes

--- src/week4/peer_graded_assignment/test_diferentes_tipos_produtos/TestProdutoComTamanho.java
+++ src/week4/peer_graded_assignment/test_diferentes_tipos_produtos/TestProdutoComTamanho.java
@@ -10,13 +10,6 @@ import org.junit.jupiter.api.Test;
 
 class TestProdutoComTamanho {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
 
 	@BeforeEach
 	void setUp() throws Exception {
